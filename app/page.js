8b67296a5fc7d052701549679c262a6aab826614
akgupta2002D <akgupta2002d@gmail.com>
2025-05-05 03:53:00 -0400

    Remove CAWebsite to Boys

--- app/page.js
+++ app/page.js
@@ -1,34 +1,11 @@
 // app/page.js
 import React from 'react'
-import { AppBar, Toolbar, Typography, Container } from '@mui/material'
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions'
-import BlogPost from './blogComponents/BlogPost'
-import { getPosts } from './blogLib/getPosts'
 
 export default function Home () {
-  const posts = getPosts() // Call getPosts directly in the component
 
   return (
     <>
-      <AppBar position='static' color='primary'>
-        <Toolbar>
-          <EmojiEmotionsIcon sx={{ mr: 2 }} />
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            Reslife Blog - Jonsson & Wiecking RAs
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container sx={{ padding: { xs: 2, md: 4 } }}>
-        {posts.map((post, index) => (
-          <BlogPost
-            key={index}
-            index={index}
-            title={post.title}
-            content={post.content}
-            image={`/${post.image}`} // Prefix with slash to use public path
-          />
-        ))}
-      </Container>
+      <h2>For Boys!</h2>
     </>
   )
 }
